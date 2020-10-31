c134d45de56a7e26f523ce94f08d8c57f56d32a0
jarumon <72619607+jarumon@users.noreply.github.com>
2020-10-31 10:41:54 +0700

    Add files via upload

---
+++ atm/src/main/java/th/ac/ku/atm/service/BCrypt.java
@@ -0,0 +1,12 @@
+package th.ac.ku.atm.service;
+
+public class BCrypt {
+    public static String gensalt(int i) {
+    }
+
+    public static String hashpw(String pin, String salt) {
+    }
+
+    public static boolean checkpw(String pin, String hashPin) {
+    }
+}
